123e3ba8b83ae5d1f9177d56c6d3082461c4c996
Andy Fischer <info@andyland.info>
2020-06-03 22:51:15 +0200

    show only their messages

--- WhatsAppMessageScraper.py
+++ WhatsAppMessageScraper.py
@@ -13,32 +13,37 @@ name = ''
 msgText = ''
 
 
-while True:
-    elemContact = web.find_elements_by_xpath('//span[@class="_3TEwt"]')
+def getMessage(pContact):
+    try:
+        #print("Lokation:" + pContact.location)
+        #contact = elemContact[x]
+        txtName = pContact.text
+        pContact.click()
+        elemText = web.find_elements_by_xpath(
+            '//span[@class="_3FXB1 selectable-text invisible-space copyable-text"]')
+        #elemText = web.find_elements_by_class_name("_3_7SH _3DFk6")
 
-    for contact in elemContact:
-        # x.click()
-        #print("Name:" + contact.text)
-        try:
-            txtName = contact.text
-            contact.click()
-            elemText = web.find_elements_by_xpath(
-                '//span[@class="_3FXB1 selectable-text invisible-space copyable-text"]')
+        # '//span[@class="_3FXB1 selectable-text invisible-space copyable-text"]')
         # for msg in elemText:
         #    print('Text:' + msg.text)
-        except:
-            pass
+        #y_relative_coord = elemText.location['y']
+        #print("posY:" + y_relative_coord)
+    except:
+        pass
 
-        try:
-            bNewMsg = False
-            n = len(elemText)
-            msgText = elemText[n-1].text
-            #print('Last msg:' + msgText)
-            if msgText.startswith('.'):
-                if txtName in contactDict:
-                    # ist der letzte text von einem bekannten kontakt und neu?
-                    storedMsg = contactDict[txtName]
-                    if msgText != storedMsg:
+    try:
+        bNewMsg = False
+        n = len(elemText)
+        msgText = elemText[n-1].text
+        #print('Last msg:' + msgText)
+        loca = elemText[n-1].location
+        print(loca['x'])
+        if not msgText.startswith('.'):
+            if txtName in contactDict:
+                # ist der letzte text von einem bekannten kontakt und neu?
+                storedMsg = contactDict[txtName]
+                if msgText != storedMsg:
+                    if loca['x'] < 600:
                         # dann ersetzen
                         contactDict[txtName] = msgText
                         bNewMsg = True
@@ -46,24 +51,38 @@ while True:
                         #chatFile = open("/Users/andyfischer/Desktop/chat.txt", "a")
                         #chatFile.write(txtName + ':' + msgText + "\n\n")
                         # chatFile.close()
-                    # else:
-                        #print('Keine neue MSG von ' + txtName)
-                else:
-                    # dem dictionary hinzufuegen
+                # else:
+                    #print('Keine neue MSG von ' + txtName)
+            else:
+                # dem dictionary hinzufuegen
+                #print(txtName + ":" + msgText)
+                if loca['x'] < 600:
                     contactDict[txtName] = msgText
                     bNewMsg = True
 
-                if bNewMsg:
-                    txtInitial = txtName[0:4]
+            if bNewMsg:
+                txtInitial = txtName[0:4]
 
-                    chatFile = open("/Users/andyfischer/Desktop/chat.txt", "a")
-                    #chatFile.write(txtName + ':' + msgText + "\n\n")
-                    chatFile.write(txtInitial + ':' + msgText[0:50] + "\n\n")
-                    chatFile.close()
-                    print(txtInitial + ':' + msgText)
-        except:
-            pass
-        # finally:
-            # print('------')
-    print('warte 5 Sekunden')
-    time.sleep(5)
+                chatFile = open("/Users/andyfischer/Desktop/chat.txt", "a")
+                #chatFile.write(txtName + ':' + msgText + "\n\n")
+                chatFile.write(txtInitial + ':' + msgText[0:50] + "\n\n")
+                chatFile.close()
+                print(txtInitial + ':' + msgText)
+    except:
+        pass
+    # finally:
+        # print('------')
+    return
+
+
+while True:
+    elemContact = web.find_elements_by_xpath('//span[@class="_3TEwt"]')
+    iLen = len(elemContact)
+    for x in range(iLen-4, iLen):
+        # for contact in elemContact:
+        # x.click()
+        #print("Name:" + contact.text)
+        getMessage(elemContact[x])
+    getMessage(elemContact[0])
+    #print('warte 2 Sekunden')
+    # time.sleep(2)
